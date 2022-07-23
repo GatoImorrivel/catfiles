7d5582086aa6568186531170f156f2ab474c7f7e
GatoImorrivel <guilhermevianagui23@gmail.com>
2022-07-23 15:32:09 -0300

    save

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -7,6 +7,14 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
-polybar -r -c $HOME/.config/polybar/config.ini mybar &
-sleep 1
-polybar -c $HOME/.config/polybar/config.ini separator &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -r -c $HOME/.config/polybar/config.ini mybar &
+    sleep 1
+    MONITOR=$m polybar -r -c $HOME/.config/polybar/config.ini separator &
+  done
+else
+  polybar -r -c $HOME/.config/polybar/config.ini mybar &
+  sleep 1
+  polybar -r -c $HOME/.config/polybar/config.ini separator &
+fi
